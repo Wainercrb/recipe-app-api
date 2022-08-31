e32c2f952a3727412dae7ab853a37135d99cd3b7
Wainer <wainer.rodriguez@omni.cr>
2022-08-31 16:09:40 -0600

    Added Wait for db test and conf

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
+
 def add(a, b):
-  """Add a and b return result."""
-  return a + b
+    """Add a and b return result."""
+    return a + b
+
 
 def subtract(a, b):
-  """Subtract a and b and return result."""
-  return b - a
+    """Subtract a and b and return result."""
+    return b - a
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,25 +1,24 @@
 """
-Simple tests
+Simple tests.
 """
 
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-  """Test the calc module"""
+    """Test the calc module."""
 
-  def test_add_numbers(self):
-    """Test adding number together"""
+    def test_add_numbers(self):
+        """Test adding number together."""
 
-    res = calc.add(5, 1)
+        res = calc.add(5, 1)
 
-    self.assertEqual(res, 6)
+        self.assertEqual(res, 6)
 
-  def test_subtract_numbers(self):
-    """Test subtracting number together"""
+    def test_subtract_numbers(self):
+        """Test subtracting number together."""
 
-    res = calc.subtract(10, 15)
+        res = calc.subtract(10, 15)
 
-    self.assertEqual(res, 5)
-
-
+        self.assertEqual(res, 5)
