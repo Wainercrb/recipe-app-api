7e423e56d6d150f3d4120ced58d4a1ccdb647ab7
Wainer <wainer.rodriguez@omni.cr>
2022-09-01 13:54:27 -0600

    Added email normalized def

--- app/core/models.py
+++ app/core/models.py
@@ -13,24 +13,24 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
 
-        user = self.model(email = email, **extra_fields)
+        normalized_email = self.normalize_email(email)
+        user = self.model(email=normalized_email, **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
 
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,15 +6,29 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTest(TestCase):
-      """Test models."""
+    """Test models."""
 
-      def test_create_user_with_email_successful(self):
-          email = 'test@example.com'
-          password = '123'
-          user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
-          )
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = '123'
+        user = get_user_model().objects.create_user(
+          email=email,
+          password=password,
+        )
 
-          self.assertEqual(user.email, email)
-          self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+
+        sample_emails = [
+          ['test1@EXAMPLE.com', 'test1@example.com'],
+          ['Test2@Example.com', 'Test2@example.com'],
+          ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+          ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
